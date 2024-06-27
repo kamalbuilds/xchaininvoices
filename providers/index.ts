ca66872088ea011e044c487f98ab4c599ff753aa
Kamal Nayan <geniusamansingh@gmail.com>
2024-06-27 17:04:55 +0530

    install shadcn

---
+++ providers/index.ts
@@ -0,0 +1,3 @@
+import { Web3Provider } from './web3-provider';
+
+export { Web3Provider };
---
+++ providers/web3-provider.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import type { PropsWithChildren } from 'react';
+
+import { projectId, wagmiConfig } from '~/lib/viem';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { createWeb3Modal } from '@web3modal/wagmi/react';
+import { type State, WagmiProvider } from 'wagmi';
+
+createWeb3Modal({
+  wagmiConfig,
+  projectId,
+  enableAnalytics: true,
+  themeMode: 'light',
+});
+
+const queryClient = new QueryClient();
+
+interface Web3ProviderProps extends PropsWithChildren {
+  initialState?: State;
+}
+
+export const Web3Provider = ({ children, initialState }: Web3ProviderProps) => {
+  return (
+    <WagmiProvider config={wagmiConfig} initialState={initialState}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </WagmiProvider>
+  );
+};
