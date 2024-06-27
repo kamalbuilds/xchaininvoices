dc07943646e6dfd1af2aaab2bcec93e146aeb940
Kamal Nayan <geniusamansingh@gmail.com>
2024-06-27 17:44:55 +0530

    walletconnect connectwallet

---
+++ lib/viem/index.ts
@@ -0,0 +1,34 @@
+import {
+  type Config,
+  cookieStorage,
+  createConfig,
+  createStorage,
+  http,
+} from 'wagmi';
+import { anvil, mainnet, sepolia, zkSyncSepoliaTestnet } from 'wagmi/chains';
+import { walletConnect } from 'wagmi/connectors';
+import { env } from '~/env';
+
+export const projectId = env.NEXT_PUBLIC_WALLETCONNECT_ID;
+
+const metadata = {
+  name: 'Web3 Turbo Starter',
+  description: 'Web3 starter kit with turborepo, wagmi, and Next.js',
+  url: 'http://localhost:3000',
+  icons: ['https://avatars.githubusercontent.com/u/37784886'],
+};
+
+export const wagmiConfig: Config = createConfig({
+  chains: [mainnet, sepolia, anvil, zkSyncSepoliaTestnet],
+  ssr: true,
+  storage: createStorage({
+    storage: cookieStorage,
+  }),
+  connectors: [walletConnect({ projectId, metadata, showQrModal: false })],
+  transports: {
+    [mainnet.id]: http(),
+    [sepolia.id]: http(),
+    [anvil.id]: http(),
+    [zkSyncSepoliaTestnet.id]: http(),
+  },
+});
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,87 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
-const config: Config = {
+const config = {
+  darkMode: ['class'],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{ts,tsx,md,mdx}',
+    './components/**/*.{ts,tsx,md,mdx}',
+    './app/**/*.{ts,tsx,md,mdx}',
+    './**/*.{ts,tsx,md,mdx}',
   ],
+  prefix: '',
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        sans: ['var(--font-sans)'],
+        sfProRegular: ['SF Pro Regular', 'sans-serif'],
+        sfProMedium: ['SF Pro Medium', 'sans-serif'],
+        sfProSemibold: ['SF Pro Semibold', 'sans-serif'],
+        sfProBold: ['SF Pro Bold', 'sans-serif'],
+      },
+      colors: {
+        border: 'var(--border)',
+        input: 'var(--input)',
+        ring: 'var(--ring)',
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        primary: {
+          DEFAULT: 'var(--primary)',
+          foreground: 'var(--primary-foreground)',
+        },
+        secondary: {
+          DEFAULT: 'var(--secondary)',
+          foreground: 'var(--secondary-foreground)',
+        },
+        destructive: {
+          DEFAULT: 'var(--destructive)',
+          foreground: 'var(--destructive-foreground)',
+        },
+        muted: {
+          DEFAULT: 'var(--muted)',
+          foreground: 'var(--muted-foreground)',
+        },
+        accent: {
+          DEFAULT: 'var(--accent)',
+          foreground: 'var(--accent-foreground)',
+        },
+        popover: {
+          DEFAULT: 'var(--popover)',
+          foreground: 'var(--popover-foreground)',
+        },
+        card: {
+          DEFAULT: 'var(--card)',
+          foreground: 'var(--card-foreground)',
+        },
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
     },
   },
-  plugins: [],
-};
+  plugins: [require('tailwindcss-animate')],
+} satisfies Config;
+
 export default config;
